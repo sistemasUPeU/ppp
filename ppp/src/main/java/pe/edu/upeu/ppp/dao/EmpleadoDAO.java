e06c0e7bf28628bddced45d6eaeac551cd03cb09
Mogollón Calle Kevin Jr <Kevin1097@users.noreply.github.com>
2017-11-28 23:20:51 -0500

    aaa
    aaa

---
+++ ppp/src/main/java/pe/edu/upeu/ppp/dao/EmpleadoDAO.java
@@ -0,0 +1,9 @@
+package pe.edu.upeu.ppp.dao;
+
+import java.util.List;
+import java.util.Map;
+
+public interface EmpleadoDAO {
+	List<Map<String, Object>> lista ();
+
+}
---
+++ ppp/src/main/java/pe/edu/upeu/ppp/daoImp/EmpleadoImp.java
@@ -0,0 +1,16 @@
+package pe.edu.upeu.ppp.daoImp;
+
+import java.util.List;
+import java.util.Map;
+
+import pe.edu.upeu.ppp.dao.EmpleadoDAO;
+
+public class EmpleadoImp implements EmpleadoDAO {
+
+	@Override
+	public List<Map<String, Object>> lista() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
