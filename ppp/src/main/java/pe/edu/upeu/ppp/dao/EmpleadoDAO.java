205d2562eab9de45ee95f6110f98fce6b0cdef63
Mogollón Calle Kevin Jr <Kevin1097@users.noreply.github.com>
2017-11-29 04:04:16 -0500

    aa
    aa

--- ppp/src/main/java/pe/edu/upeu/ppp/dao/EmpleadoDAO.java
+++
@@ -1,5 +0,0 @@
-package pe.edu.upeu.ppp.dao;
-
-public class EmpleadoDAO {
-
-}
---
+++ ppp/src/main/java/pe/edu/upeu/ppp/dao/RepresentanteDAO.java
@@ -0,0 +1,11 @@
+package pe.edu.upeu.ppp.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class RepresentanteDAO {
+
+	@Autowired
+	JdbcTemplate jdbc;
+	
+}
