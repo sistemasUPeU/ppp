0df11b13fc17534e33285ea1a2fee604d23cd32e
Mogollón Calle Kevin Jr <Kevin1097@users.noreply.github.com>
2017-11-29 04:16:44 -0500

    contoller
    controller

--- ppp/src/main/java/pe/edu/upeu/ppp/controller/CenterController.java
+++ ppp/src/main/java/pe/edu/upeu/ppp/controller/CenterController.java
@@ -1,10 +1,69 @@
 package pe.edu.upeu.ppp.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.google.gson.Gson;
+
+
 
 @Controller
 public class CenterController {
+	@Autowired
+	List<Map<String, Object>> listas;
+	@RequestMapping("/paginas")
+	public ModelAndView paginas(ModelAndView modelo,HttpServletRequest request,HttpServletResponse response)
+	{
+		String opc=request.getParameter("opc");
+		switch (opc) {
+		case "1":
+			break;
+
+		case"2":
+			break;
+		}
+		return modelo;
+	}
 	
+	@RequestMapping(value="/proc",method=RequestMethod.POST)
+	public void metod(HttpServletRequest request,HttpServletResponse response) throws IOException
+	{
+		response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        String opc = request.getParameter("opc");
+        try {
+        	switch (opc) {
+			case "convenio":
+				
+			
+				break;
+
+			case "estplanilla":
+
+				break;
+			}
+				
+			
+		} catch (Exception e) {
+
+			System.out.println("Error:"+e);
+		}
+        Gson gson = new Gson();
+        out.println(gson.toJson(listas));
+		out.flush();
+		out.close();
+	}
 	
 
 }
