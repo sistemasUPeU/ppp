b027e8f062e92a2c22456030af0fd8366f8005d9
Brandux <branduxjuarez@upeu.edu.pe>
2017-12-06 12:30:12 -0500

    sas

--- ppp/src/main/webapp/resources/js/getPerfil.js
+++ ppp/src/main/webapp/resources/js/getPerfil.js
@@ -106,9 +106,11 @@ function getrutas(id){
 function namePerfil(nombre ,apellido ) {
 	var s = '<a class="dropdown-toggle" data-toggle="dropdown">';
 	s+='<img src=\"<c:url value=\'resources/imagenes/1.png\'/>" alt=\"">';
+	s+='<img src="<c:url value='|'resources/imagenes/1.png'|'/>" alt="">';
 	s+='<span>'+nombre+'  '+apellido+'</span>';
 	s+='<i class="caret"></i>';
 	s+='</a>';
 	return s;
 }
 
+}
